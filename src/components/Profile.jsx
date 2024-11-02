899f6e19e75d9c720c3455595fa52245b5624e80
IrynaKhonina <irinahonina@yahoo.com>
2024-11-02 16:10:50 +0300

    first

---
+++ src/components/Profile.jsx
@@ -0,0 +1,39 @@
+
+
+
+
+export const Profile = () => {
+
+    return (
+        <>
+           <div className='content'>
+        <div>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQovFzI1OM5Y_eqSiGhVsYwVMOaOr1UDQxBvg&s' />
+        </div>
+      <div>
+        ava+ description
+      </div>
+
+      <div>
+        my posts
+        <div>
+          new post
+        </div>
+
+
+        <div>
+          <div>
+            1
+          </div>
+
+          <div>
+            2
+          </div>
+        </div>
+      </div>
+      </div>
+        </>
+    )
+}
+
+
