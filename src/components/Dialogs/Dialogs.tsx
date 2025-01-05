00e11d7d322d93a9da406c5d4fe4ee31fbaec0ac
IrynaKhonina <irinahonina@yahoo.com>
2025-01-05 15:27:22 +0300

    22

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,40 +1,51 @@
-import s from './Dialogs.module.css';
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
+import s from './Dialogs.module.css';
 
+// Типизация для пропсов DialogItem
+type DialogItemProps = {
+    name: string; // Добавляем пропс name
+};
+
+// Компонент для отображения одного диалога
+const DialogItem: React.FC<DialogItemProps> = ({ name }) => {
+    return (
+        <div className={`${s.dialog} ${s.active}`}>
+            <Link to="/dialogs/1">{name}</Link>
+        </div>
+    );
+};
+
+// Компонент для отображения списка диалогов
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
+                {/* Передаем пропс name в DialogItem */}
+                <DialogItem name={"Aleksandr"} />
 
-                <div className={`${s.dialog} ${s.active}`}>
-                    <Link to ="/dialogs/1">Aleksandr</Link>
+                <div className={s.dialog}>
+                    <Link to="/dialogs/2">Boris</Link>
                 </div>
                 <div className={s.dialog}>
-                    <Link to ="/dialogs/2">Boris</Link>
+                    <Link to="/dialogs/3">Iryna</Link>
                 </div>
                 <div className={s.dialog}>
-                    <Link to ="/dialogs/3">Iryna</Link>
+                    <Link to="/dialogs/4">Elena</Link>
                 </div>
                 <div className={s.dialog}>
-                    <Link to ="/dialogs/4">Elena</Link>
+                    <Link to="/dialogs/5">Ivan</Link>
                 </div>
                 <div className={s.dialog}>
-                    <Link to ="/dialogs/5">Ivan</Link>
+                    <Link to="/dialogs/6">Alexey</Link>
                 </div>
-                <div className={s.dialog}>
-                    <Link to ="/dialogs/6">Alexey</Link>
-                </div>
-
             </div>
 
-
             <div className={s.messages}>
-            <div className={s.dialog}>Hi</div>
-            <div className={s.dialog}>How are you?</div>
-            <div className={s.dialog}>Yo</div>
-        </div>
-
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How are you?</div>
+                <div className={s.dialog}>Yo</div>
+            </div>
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom'; // Используем HashRouter для GitHub Pages
 
+// Создаем корневой элемент для рендеринга
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
+// Рендерим приложение
 root.render(
     <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+        <HashRouter> {/* Оборачиваем приложение в HashRouter */}
+            <App />
+        </HashRouter>
     </React.StrictMode>
 );
 
+// Отчет о производительности (опционально)
 reportWebVitals();
