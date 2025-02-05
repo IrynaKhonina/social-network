9aa292c0656ced7e4041312855bd95dbc47fb0ab
IrynaKhonina <irinahonina@yahoo.com>
2025-02-05 11:01:13 +0300

    27

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './App.css';
 import { Header } from './components/Header/Header';
 import { Navbar } from './components/Navbar/Navbar';
 import { Dialogs } from "./components/Dialogs/Dialogs";
-import { Route, Routes  } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Profile } from "./components/Profile/Profile";
+
+let SomeComponent = () => <Dialogs />;
 
 function App() {
     return (
@@ -13,9 +15,9 @@ function App() {
             <Navbar />
             <div className="app-wrapper-content">
                 <Routes>
-                    <Route path="/dialogs" element={<Dialogs />} />
+                    {/* Передаем JSX-элемент SomeComponent вместо компонента как функции */}
+                    <Route path="/dialogs" element={<SomeComponent />} />
                     <Route path="/profile" element={<Profile />} />
-
                 </Routes>
             </div>
         </div>
